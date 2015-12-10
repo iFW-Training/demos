f36b9b04b3afa40275cb570947720923dadc06d3
admin <casey.trauer@interfaceware.com>
2015-12-10 16:36:44 +0000

    Export: Overwrite with working version.

--- shared/node.lua
+++ shared/node.lua
@@ -1,23 +1,8 @@
-
+-- Utilities for "node" Values
 
 -- Coerce a node value into a string.
 --
 function node:S()
    return tostring(self)
 end
-function node:V()
-   return self:nodeValue()
-end
-  -- round node real number value to count of decimal places function       
-function node.round(self, p)        
-   local o = 10^(p or 0)       
-   return math.floor(self*o+0.5)/o    
-end  
-function node.childIndex(Node, Name)
-   for i=1, #Node do
-      if Node[i]:nodeName() == Name then 
-         return i 
-      end
-   end  
-   return nil
-end
+
